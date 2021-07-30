f8f859600d0740e6937b335894a44ab1f16cc7b6
Vladimir Vorobiev <vvs-it@bk.ru>
2021-07-30 17:38:15 +0600

    add var & (alert) $ console.log

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// Создаем переменные
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
+
+// Вызываем (alert)
+alert("ДЗ сдано!");
+
+// Передаем сообщение в консоль
+console.log("ДЗ сдано!");
