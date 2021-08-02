c538e0db4a020417178d628dc1056c73dafdc804
Vladimir Vorobiev <vvs-it@bk.ru>
2021-08-03 03:07:38 +0600

    lesson03 ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+'use strict';
+
+// Объявляем пременные
 let money = 55000;
 let income = 'фриланс';
 let addExpenses = 'Гсм, Спортпит, Обучение';
@@ -9,10 +12,49 @@ let budgetDay = money / 30;
 // Вызываем (alert)
 alert('ДЗ сдано!');
 
-// Передаем сообщение в консоль
+// Перетираем значения объявленных переменных
+money = +prompt('Ваш месячный доход?');
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+deposit = confirm('Есть ли у вас депозит в банке?');
+let expenses1 = prompt('Введите обязательную статью раходов.');
+let amount1 = +prompt('Во сколько это обойдется?');
+let expenses2 = prompt('Введите обязательную статью раходов.');
+let amount2 = +prompt('Во сколько это обойдется?');
+
+// Объявляем-перетираем переменые
+let budgetMonth = money - (amount1 + amount2);
+let missionMonth = Math.ceil(mission / budgetMonth);
+budgetDay = Math.floor(budgetMonth / 30);
+
+// Передаем данные в консоль
 console.log('ДЗ сдано!');
 console.log('Период равен', period, 'месяцев и цель заработать', mission, 'рублей.');
 console.log(budgetDay);
+console.log(money);
+console.log(addExpenses);
+console.log(deposit);
+console.log(expenses1);
+console.log(amount1);
+console.log(expenses2);
+console.log(amount2);
+console.log(budgetMonth);
+console.log(missionMonth);
+console.log(budgetDay);
+
+// Конструкция условий
+if (budgetDay > 1200) {
+  console.log('У вас высокий уровень дохода');
+} else if (budgetDay === 1200) {
+  console.log('Ваш уровень дохода выше среднего');
+} else if (budgetDay > 600) {
+  console.log('У вас средний уровень дохода');
+} else if (budgetDay === 600) {
+  console.log('Ваш уровень дохода выше низкого');
+} else if (budgetDay > 0) {
+  console.log('К сожалению у вас уровень низкий уровень дохода');
+} else {
+  console.log('Что то пошло не так');
+}
 
 // Выводим тип данных в консоль
 console.log(typeof money);
