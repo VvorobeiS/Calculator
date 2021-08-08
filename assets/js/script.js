a32fbe9361da1ea100fa512dbbb271578f7bf06a
Vladimir Vorobiev <vvs-it@bk.ru>
2021-08-08 19:20:36 +0600

    lesson05 revisions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,32 @@
 'use strict';
 
-// Объявляем пременные
+// Функция для проверки введеного типа данных
+const isNumber = function (n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+// Получаем месячный доход
 let money;
-let income = 'фриланс';
+const start = function () {
+  do {
+    money = prompt('Ваш месячный доход?', 55000);
+  } while (!isNumber(money));
+
+  return money;
+};
+money = start();
+
+// Узнаем о наличии депозита
 let deposit = confirm('Есть ли у вас депозит в банке?');
+
+// Узнаем возможные расходы
 let addExpenses = prompt(
   'Перечислите возможные расходы за рассчитываемый период через запятую',
   'Одежда, спортинвертарь, отдых'
 );
+
+// Объявляем пременные
+let income = 'фриланс';
 let mission = 500000;
 let period = 12;
 let expenses = [];
@@ -23,27 +42,17 @@ showTypeOf(deposit);
 // Приводим строку к нижнему регистру, разбиваем на массив и выводим в консоль
 console.log(addExpenses.toLowerCase().split(', '));
 
-// Функция для проверки введеного типа данных
-const isNumber = function (n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-};
-
-// Получаем месячный доход
-const start = function () {
-  do {
-    money = prompt('Ваш месячный доход?', 55000);
-  } while (!isNumber(money));
-
-  return money;
-};
-money = start();
-
 // Получаем обязательные статьи расходов и их сумму
 const getExpensesMonth = function () {
+  let num = 0;
   let sum = 0;
   for (let i = 0; i < 2; i++) {
     expenses[i] = prompt('Введите обязательную статью раходов.');
-    sum += +prompt('Во сколько это обойдется?', 5000);
+    num = prompt('Во сколько это обойдется?', 5000);
+    while (!isNumber(num)) {
+      num = prompt('Во сколько это обойдется?', 5000);
+    }
+    sum += Number(num);
   }
   return sum;
 };
